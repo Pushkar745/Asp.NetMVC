86109472690ed9cc29785892edec9f505f30a983
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-03-16 21:50:51 +0530

    Create CRUD operation

---
+++ MVCCRUDOperation/MVCCRUDOperation/Controllers/CRUDDemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCRUDOperation.Controllers
+{
+    public class CRUDDemoController : Controller
+    {
+        // GET: CRUDDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
